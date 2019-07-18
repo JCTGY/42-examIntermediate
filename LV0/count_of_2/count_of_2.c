c90bfc7be63445712edde8e9f7dd4d88f177b280
JCTGY <fan3510@gmail.com>
2019-07-18 11:07:57 -0700

    clean up count_of_2.c

--- LV0/count_of_2/count_of_2.c
+++ LV0/count_of_2/count_of_2.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   count_of_2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchiang- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/18 10:03:41 by jchiang-          #+#    #+#             */
+/*   Updated: 2019/07/18 11:07:12 by jchiang-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -20,23 +32,3 @@ int main(int argc, char **argv){
     printf("\n");
     return (0);
 }
-
-
-/*
-COUNT_OF_2
-Assignment name  : count_of_2
-Expected files   : count_of_2.c
-Allowed functions: None
---------------------------------------------------------------------------------
-Implement a function which counts, for a given integer n, the number of 2s
-that appear in all the numbers between 0 and n (inclusive).
-Your function must be declared as follows:
-int    count_of_2(int n);
-If n <= 1, the function returns 0;
-Examples:
-input  = 25
-output =  9
-because there are 9 2s in (2, 12, 20, 21, 22, 23, 24 and 25)
-Note:
-the number 22 counts as 2 because it has two 2s, number 202 count as 2, number 22022 count as 4 etc…
-*/
