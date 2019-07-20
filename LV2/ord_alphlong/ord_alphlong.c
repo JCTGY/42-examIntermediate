d31da2a730c6110cbc29796e3b72018e77215350
JCTGY <fan3510@gmail.com>
2019-07-20 11:29:17 -0700

    need to fix oral

--- LV2/ord_alphlong/ord_alphlong.c
+++ LV2/ord_alphlong/ord_alphlong.c
@@ -1,5 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ord_alphlong.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchiang- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/20 10:38:19 by jchiang-          #+#    #+#             */
+/*   Updated: 2019/07/20 11:28:35 by jchiang-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+# define MAX(a, b) ((a > b) ? a : b);
 
 void ft_putchar(char c){
     write(1, &c, 1);
@@ -17,12 +32,92 @@ int ft_strlen(char *str){
     return (i);
 }
 
-# define MAX(a, b) ((a > b) ? a : b);
+int is_space(char c){
+	return (c == ' ' || c == '\t');
+}
 
-//typedef struct s_word{
-//    int len;
-//    char *str;
-//    struct s_word *next;
-//}t_word;;
+int word_maxlen(char *s){
 
+	int max_len = 0;
+	int tmp = 0;
+	for (int i = 0; s[i]; i++){
+		if (!is_space(s[i]))
+			tmp++;
+		else if (s[i + 1] == '\0') {
+			if (tmp > max_len)
+				max_len = tmp;
+		}
+		else {
+			if (tmp > max_len)
+				max_len = tmp;
+			tmp = 0;
+		}
+	}
+	return (max_len);
+}
 
+int word_count(char *s){
+
+	int count = 0;
+	for (int i = 0; s[i]; i++){
+		if (!is_space(s[i]) && (is_space(s[i + 1]) || s[i + 1] == '\0'))
+			count++;
+	}
+	return (count);
+}
+
+char *word_sep(char *s, int *i, int max_len){
+
+	char *tmp;
+	int p = 0;
+	int x = *i;
+
+	tmp = malloc(sizeof(*tmp) * max_len);
+	while (is_space(s[x]))
+		x++;
+	while (!is_space(s[x]) && s[x])
+		tmp[p++] = s[x++];
+	tmp[p] = '\0';
+	*i = x;
+	printf("waht is tmp = %s\n", tmp);
+	return (tmp);
+}
+
+void ft_strcpy(char *s1, char *s2){
+
+	int p = 0;
+	for (int i = 0; s2[i]; i++)
+		s1[p++] = s2[i];
+	s1[p] = '\0';
+}
+
+void ord_alph(char *s){
+
+	int word = word_count(s);
+	int max_len = word_maxlen(s) + 1;
+	printf("word_count = %d\n", word);
+	printf("max_len = %d\n", max_len);
+	char **arr = malloc(sizeof(*arr) * word + 1);
+	arr[word] = NULL;
+	for (int w = 0; w < word; w++){
+		for (int i = 0; s[i]; i++){
+			arr[w] = malloc(sizeof(**arr) * max_len);
+			char *tmp = word_sep(s, &i, max_len);
+			ft_strcpy(arr[w], tmp);
+			if (s[i] == '\0')
+				break ;
+		}
+	}
+	for (int i = 0; arr[i]; i++){
+		ft_putstr(arr[i]);
+		ft_putchar('\n');
+	}
+}	
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		ord_alph(argv[1]);
+	ft_putchar('\n');
+	return (0);
+}
