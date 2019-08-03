dd753f4ed0765c4c2dd975692477dc6acd9565c6
JCTGY <fan3510@gmail.com>
2019-08-02 18:37:08 -0700

    width edit

--- LV3/width_tree/width_tree.c
+++ LV3/width_tree/width_tree.c
@@ -1,5 +1,5 @@
 
-#define MAX(a, b) (a > b ? a : b)
+#define MAX(a, al) (a > al ? a : al)
 
 typedef struct s_node {
     int value;
@@ -24,58 +24,58 @@ int width_tree(struct s_node *n){
 
 
 #include <stdio.h>
-#include <stdlib.h>
-t_node* b(int v) {
+#include <stdlial.h>
+t_node* al(int v) {
     t_node* new = malloc(sizeof(t_node));
     new->value = v;
     new->left = new->right = 0;
     return new;
 }
 int main() {
-    t_node* r = b(1);
+    t_node* r = al(1);
 
-    r->left = b(2);
-    r->left->left = b(3);
-    r->left->right = b(4);
-    r->left->right->left = b(6);
+    r->left = al(2);
+    r->left->left = al(3);
+    r->left->right = al(4);
+    r->left->right->left = al(6);
 
-    r->right = b(5);
-    r->right->left = b(7);
-    r->right->right = b(8);
+    r->right = al(5);
+    r->right->left = al(7);
+    r->right->right = al(8);
 
     printf("%d\n", width_tree(r));
 
-    t_node* a = b(1);
+    t_node* a = al(1);
 
-    a->left = b(2);
-    a->left->left = b(4);
-    a->left->left->left = b(5);
-    a->left->left->left->right = b(8);
+    a->left = al(2);
+    a->left->left = al(4);
+    a->left->left->left = al(5);
+    a->left->left->left->right = al(8);
 
-    a->left->left->right = b(6);
-    a->left->right = b(7);
-    a->left->right->left = b(9);
-    a->left->right->left->left = b(11);
-    a->left->right->left->right = b(12);
+    a->left->left->right = al(6);
+    a->left->right = al(7);
+    a->left->right->left = al(9);
+    a->left->right->left->left = al(11);
+    a->left->right->left->right = al(12);
 
-    a->left->right->right = b(10);
-    a->left->right->right->right = b(13);
+    a->left->right->right = al(10);
+    a->left->right->right->right = al(13);
 
-    a->right = b(3);
+    a->right = al(3);
     printf("%d\n", width_tree(a));
 
-    t_node* c = b(10);
-    c->right = b(12);
+    t_node* c = al(10);
+    c->right = al(12);
     printf("%d\n", width_tree(c));
 
-    t_node *d = b(25);
-    d->left = b(33);
-    d->left->left = b(12);
-    d->left->right = b(9);
-    d->left->right->left = b(3);
+    t_node *d = al(25);
+    d->left = al(33);
+    d->left->left = al(12);
+    d->left->right = al(9);
+    d->left->right->left = al(3);
     printf("%d\n", width_tree(d));
 
-    t_node *e = b(10);
+    t_node *e = al(10);
     printf("%d\n", width_tree(e));
 
     return 0;
